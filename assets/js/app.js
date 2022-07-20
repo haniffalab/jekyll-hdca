e6482aec836dabaa33bd13293f7c08b489bbb466
Dave Horsfall <dave@horsfall.dev>
2022-07-20 15:56:41 +0100

    update yolk sac collection

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,22 @@
       }, 1000)
     });
 
+      $('.video-link').click(function() {
+        $('.myVideo').attr("src", $(this).attr("vidUrl"));
+        $('.overlay').fadeIn(500, function(){
+          $('.main-vid-box').fadeIn(500);
+          $('.close').fadeIn(500);
+        });
+      });
+      
+      $('.close, .overlay').click(function() {
+        $('.overlay').fadeOut(500);
+        $('.myVideo').attr("src", $(this).attr("vidUrl"));
+        $('.main-vid-box').fadeOut(500);
+        $('.close').fadeOut(500);
+      });
+ 
+    
     // D3 icon element
     if($("#t-sne").length){
       const width = 400,
